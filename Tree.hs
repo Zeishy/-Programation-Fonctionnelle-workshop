801d93df5851ee0d71625bc6aeb4a206ed84861c
Cédric <cedric.hoarau-de-boisvilliers@epitech.eu>
2024-01-31 22:44:30 +0300

    yes

--- Tree.hs
+++ Tree.hs
@@ -23,4 +23,15 @@ mapValues (x:xs) = Value (read x) : mapValues xs
 
 buildAST :: ([String], [String]) -> ASTree
 buildAST ([], [val]) = Value (read val)
-buildAST (ops, vals) = buildExprList ops (mapValues vals)
+buildAST (ops, vals) = buildListExpr ops (mapValues vals)
+
+buildListExpr:: [String] -> [ASTree] -> ASTree
+buildListExpr [] [a] = a
+buildListExpr (x:xs) (y:y1:ys) = buildListExpr xs (buildExpr y x y1 : ys)
+
+treeT :: ASTree -> Int
+treeT (Add e1 e2) = treeT e1 + treeT e2
+treeT (Sub e1 e2) = treeT e1 - treeT e2
+treeT (Mul e1 e2) = treeT e1 * treeT e2
+treeT (Div e1 e2) = treeT e1 `div` treeT e2
+treeT (Value n) = n
